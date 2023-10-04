fe4a5cc30ca6907f7a82ac0e37077b3a3d33fbf1
Zac Tolle <zactolle@outlook.com>
2023-10-04 15:50:58 -0700

    #1 Starts base home route

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,27 @@
+import { version } from '../../package.json'
+
 export default {
   title: 'Home',
 
   init () {
-    console.log(this.title)
+    this.screen.append(this.tui.box({
+      width: '100%',
+      height: '100%',
+      content: `{center}+-+-+-+-+-+-+-+-+-+-+-+-+-+
+{center}|M|o|d|e|l|P|r|o|m|p|t|e|r|
+{center}+-+-+-+-+-+-+-+-+-+-+-+-+-+
+{center}{bold}v${version}{/}`,
+      tags: true,
+      border: {
+        type: 'line'
+      },
+      style: {
+        fg: 'white',
+        bg: 'blue',
+        border: {
+          fg: '#f0f0f0'
+        }
+      }
+    }))
   }
 }
--- src/tui.js
+++ src/tui.js
@@ -2,11 +2,29 @@ var blessed = require('blessed')
 
 export default class {
   constructor (opts) {
-    this.title = opts.title || 'Untitled'
+    Object.assign(this, opts)
+
+    // Setup the blessed instance
+    this.tui = blessed
+    this.screen = blessed.screen({
+      title: opts.title || 'Untitled',
+      smartCSR: this.smartCSR || true,
+      tabSize: this.tabSize || 2,
+      autoPadding: this.autoPadding || true,
+      dockBorders: this.dockBorders || true,
+    })
+    
+    // Defaults and render
+    this.init()
     this.render()
+
+    // Exit with ESC or CTRL+C
+    this.screen.key(['escape', 'C-c'], function(ch, key) {
+      return process.exit(0)
+    })
   }
 
   render () {
-    console.log(this.title)
+    this.screen.render()
   }
 }
